978902c802e21ec0288c6429c3816d578c5cd9c9
dicky ap <dicky_a_permana@me.com>
2026-03-11 02:20:03 +0700

    - checkpoint level data

--- DAP-261003-AGP-TEST/Assets/Scripts/Runtime/Data/SO/LevelDataSO.cs
+++ DAP-261003-AGP-TEST/Assets/Scripts/Runtime/Data/SO/LevelDataSO.cs
@@ -2,18 +2,25 @@ using UnityEngine;
 
 namespace DAP.Runtime.Data
 {
-    [CreateAssetMenu(fileName = "LevelData", menuName = "DAP/New Level Data")]
+    [CreateAssetMenu(fileName = "LevelData", menuName = "DAP/Data/New Level Data")]
     public class LevelDataSO : ScriptableObject
     {
-        public string levelName;
+        [SerializeField] private string _levelId;
+        [SerializeField] private string _levelName;
 
         [Header("Grid Settings")]
         [Tooltip("X = Number of Columns, Y = Number of Rows")]
-        public Vector2 layout = new(2,2);
+        [SerializeField] private Vector2 _gridLayout = new(2,2);
 
         [Header("Level Rules")]
-        public float timeLimit = 30;
-        public float memorizeTime = 1;
-        public float maxMistake = 5;
+        [SerializeField] private float _timeLimit = 30;
+        [SerializeField] private float _memorizeTime = 1;
+        [SerializeField] private float _maxMistake = 5;
+
+        public string GetLevelName() => _levelName;
+        public Vector2 GetGridLayout() => _gridLayout;
+        public float GetTimeLimit() => _timeLimit;
+        public float GetMemorizeTime() => _memorizeTime;
+        public float GetMaxMistake() => _maxMistake;
     }
 }
